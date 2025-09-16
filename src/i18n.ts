7763922825962bb29383d9993a1e220d8a4b565f
etamisie <32704830+ElsaTam@users.noreply.github.com>
2025-09-16 12:09:07 -0400

    Rename i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import * as en from '../i18n/en.json';
 import * as fr from '../i18n/fr.json';
 import * as zh from '../i18n/zh.json';
-i18next.addResourceBundle('en', 'extended-graph', en);
-i18next.addResourceBundle('fr', 'extended-graph', fr);
-i18next.addResourceBundle('zh', 'extended-graph', zh);
-export const t = i18next.getFixedT(null, "extended-graph");
+i18next.addResourceBundle('en', 'node-masquerade', en);
+i18next.addResourceBundle('fr', 'node-masquerade', fr);
+i18next.addResourceBundle('zh', 'node-masquerade', zh);
+export const t = i18next.getFixedT(null, "node-masquerade");
 export const tObsidian = i18next.getFixedT(null, "default");
